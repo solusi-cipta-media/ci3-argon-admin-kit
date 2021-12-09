26095988c32a338e70992687740ca4192e04507e
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2021-12-09 09:06:22 +0700

    dt query helper

--- application/helpers/datatable_query_helper.php
+++ application/helpers/datatable_query_helper.php
@@ -9,62 +9,50 @@
  */
 class DatatableQuery
 {
-  var $table;
-  var $column;
-  var $order;
-  var $select;
-  var $where;
-  var $joinq;
-  var $group_by;
-  var $like;
+  private $table;
+  private $column_orderQ = [];
+  private $column_searchQ = [];
+  private $order = ['id' => 'desc'];
+  private $selectQ;
+  private $whereQ = [];
+  private $joinq = [];
+  private $group_byQ = [];
+  private $likeQ = [];
 
-  var $ci;
-  var $db;
+  private $ci;
+  private $db;
 
-  function __construct(
-    string $table,
-    array $column_order = [],
-    array $default_order = array('user.id' => 'desc'),
-    string $select = null,
-    array $join = [],
-    array $where = [],
-    array $like = [],
-    string $group_by = null,
-  ) {
-    $this->table = $table;
-    $this->column = $column_order;
-    if ($default_order != null) {
-      $this->order = $default_order;
-    }
-    $this->select = $select;
-    $this->where = $where;
-    $this->joinq = $join;
-    $this->group_by = $group_by;
-    $this->like = $like;
+  function __construct()
+  {
     $this->ci = &get_instance();
     $this->ci->load->database();
     $this->db = $this->ci->db;
   }
 
-  private function _get_datatables_query()
+  private function _get_datatables_query($sum = null, string $sum_group_by = null)
   {
-    if ($this->select != null) $this->db->select($this->select);
+    if ($sum == null) {
+      if ($this->selectQ != null) $this->db->select($this->selectQ);
+    } else {
+      $this->db->select($sum);
+    }
+
     $this->db->from($this->table);
-    foreach ($this->where as $key => $value) {
-      $this->db->where($value);
+    foreach ($this->whereQ as $key => $value) {
+      $this->db->where($key, $value);
     }
 
     foreach ($this->joinq as $key => $value) {
       $this->db->join($value);
     }
 
-    foreach ($this->like as $key => $value) {
-      $this->db->join($value);
+    foreach ($this->likeQ as $key => $value) {
+      $this->db->like($key, $value);
     }
 
     $i = 0;
 
-    foreach ($this->column as $item) // loop column 
+    foreach ($this->column_searchQ as $item) // loop column 
     {
       if ($_POST['search']['value']) // if datatable send POST for search
       {
@@ -77,23 +65,126 @@ class DatatableQuery
           $this->db->or_like($item, $_POST['search']['value']);
         }
 
-        if (count($this->column) - 1 == $i) //last loop
+        if (count($this->column_searchQ) - 1 == $i) //last loop
           $this->db->group_end(); //close bracket
       }
-      $column[$i] = $item; // set column array variable to order processing
       $i++;
     }
 
     if (isset($_POST['order'])) // here order processing
     {
-      $this->db->order_by($column[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+      $this->db->order_by($this->column_orderQ[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
     } else if (isset($this->order)) {
       $order = $this->order;
 
       $this->db->order_by(key($order), $order[key($order)]);
     }
+
+    foreach ($this->group_byQ as $key => $value) {
+      $this->db->group_by($value);
+    }
+
+    if ($sum_group_by != null) {
+      $this->db->group_by($sum_group_by);
+    }
+  }
+  /**
+   * ini merupakan query dimana anda mengambil datatable dari table apa.
+   */
+  public function from(string $table)
+  {
+    $this->table = $table;
   }
 
+  /**
+   * jika order == [] pada request datatable maka ini akan berfungsi sebagai default order
+   */
+  public function default_order(array $order = ['id' => 'desc'])
+  {
+    $this->order = $order;
+  }
+
+  /**
+   * urutan column order, anda bisa mengisi dengan nama-nama column yang anda butuhkan,
+   * contoh parameter:
+   * ```PHP
+   * ['id', 'nama']
+   * ```
+   */
+  public function column_order(array $column_order)
+  {
+    $this->column_orderQ = $column_order;
+  }
+
+  /**
+   * urutan column search, anda bisa mengisi dengan nama-nama column yang anda butuhkan,
+   * contoh parameter:
+   * ```PHP
+   * ['id', 'nama']
+   * ```
+   */
+  public function column_search(array $column_search)
+  {
+    $this->column_searchQ = $column_search;
+  }
+
+
+  /**
+   * merupakan query `where`, jika list lebih dari 1 maka akan dilanjutkan dengan `AND` contoh parameter
+   * ```php
+   * [
+   *  "id" => "1",
+   *  "name" => "sukidi"
+   * ]
+   * ```
+   * Maka akan menghasilkan query
+   * ```sql
+   * where id = 1 and name = sukidi
+   * ```
+   */
+  public function where(array $where = [])
+  {
+    $this->whereQ = $where;
+  }
+
+  /**
+   * contoh parameter sama dengan where
+   */
+  public function like(array $like = [])
+  {
+    $this->likeQ = $like;
+  }
+
+  /**
+   * Merupakan query select seperti codeigniter, contoh
+   * `*`
+   * maka akan menghasilkan
+   * ```sql
+   * SELECT *
+   * ```
+   */
+  public function select(string $select = "*")
+  {
+    $this->selectQ = $select;
+  }
+
+  /**
+   * Merupakan query join, contoh parameter
+   * ```php
+   * [
+   *    ["study_tbl st", "st.id = student.id_study", "left"],
+   *    ["schedule_tbl sct", "sct.id = a.id_schedule", "left"]
+   * ]
+   * ```
+   */
+  public function join(array $join = [])
+  {
+    $this->joinq = $join;
+  }
+
+  /**
+   * menghitung total yang sudah terfilter
+   */
   public function count_filtered()
   {
     $this->_get_datatables_query();
@@ -101,6 +192,18 @@ class DatatableQuery
 
     return $query->num_rows();
   }
+
+  /**
+   * contoh group by: `["nama", "id"]`
+   */
+  public function group_by(array $group_by = [])
+  {
+    $this->group_byQ = $group_by;
+  }
+
+  /**
+   * menghitung semua total
+   */
   public function count_all()
   {
     $this->db->from($this->table);
@@ -108,12 +211,69 @@ class DatatableQuery
     return $this->db->count_all_results();
   }
 
-  public function show_table()
+  /**
+   * mendapatkan list datatable
+   */
+  public function get_default()
   {
     $this->_get_datatables_query();
     if ($_POST['length'] != -1)
       $this->db->limit($_POST['length'], $_POST['start']);
     $query = $this->db->get();
     return $query->result();
+  }
+
+  /**
+   * digunakan untuk mengolah data urut dari kiri ke kanan. Jika anda menambahkan sesuatu bukan dengan nama kolom, maka anda sistem akan mendeteksi sebagai custom. jika anda menambahkan custom string, anda bisa menambahkan variable column dengan `:nama_column`
+   */
+  public function get_data_sort_by(array $column)
+  {
+    $this->_get_datatables_query();
+    if ($_POST['length'] != -1)
+      $this->db->limit($_POST['length'], $_POST['start']);
+    $query = $this->db->get();
+    $data = $query->result_array();
+
+    $result = [];
+    $no = @$_POST['start'];
+    foreach ($data as $key => $value) {
+      $no++;
+      $row = [];
+      foreach ($column as $k => $v) {
+        if (isset($value[$v])) :
+          $row[] = $value[$v];
+        elseif ($v == "{numbering}") :
+          $row[] = $no;
+        else :
+          $row[] = $this->dt_regex($v);
+        endif;
+      }
+
+      $result[] = $row;
+    }
+
+    return $result;
+  }
+
+  public function get_sum_row(string $sum_select, string $sum_group_by)
+  {
+    $this->_get_datatables_query($sum_select, $sum_group_by);
+    return $this->db->get()->row();
+  }
+
+  private function dt_regex(string $v)
+  {
+    preg_match_all('/:([A-Z|a-z|0-9]+)/', $v, $matches);
+    $q = $matches[0];
+    $k = $matches[1];
+
+    foreach ($q as $kmatch => $pmatch) {
+      if (isset($value[$k[$kmatch]])) {
+        $s = $value[$k[$kmatch]];
+        $v = preg_replace("/" . $pmatch . "/i", $s, $v);
+      }
+    }
+
+    return $v;
   }
 }
